27a19278fe8734a7f4705d9f8fee7f3401c240f2
Magesh Kuppan <tkmagesh77@gmail.com>
2025-04-23 16:10:37 +0530

    channels - streaming - assignment

--- 01-concurrency/06-streaming/19-assignment.go
+++ 01-concurrency/06-streaming/19-assignment.go
@@ -10,32 +10,30 @@ import (
 
 func main() {
 	primes := genPrimes(2, 100)
-	for _, no := range primes {
+	for no := range primes {
 		fmt.Printf("Prime No : %d\n", no)
 	}
 	fmt.Println("Done")
 }
 
-func genPrimes(start, end int) []int {
-	primes := make([]int, 0)
-	wg := sync.WaitGroup{}
-	var mutex sync.Mutex
-	for no := start; no <= end; no++ {
-		wg.Add(1)
-		go func(n int) {
-			fmt.Printf("Goroutine started for [%d]\n", n)
-			defer wg.Done()
-			defer fmt.Printf("Goroutine completed for [%d]\n", n)
-			if isPrime(n) {
-				mutex.Lock()
-				{
-					primes = append(primes, n)
+func genPrimes(start, end int) chan int {
+	primes := make(chan int)
+	go func() {
+		wg := sync.WaitGroup{}
+		for no := start; no <= end; no++ {
+			wg.Add(1)
+			go func(n int) {
+				fmt.Printf("Goroutine started for [%d]\n", n)
+				defer wg.Done()
+				defer fmt.Printf("Goroutine completed for [%d]\n", n)
+				if isPrime(n) {
+					primes <- n
 				}
-				mutex.Unlock()
-			}
-		}(no)
-	}
-	wg.Wait()
+			}(no)
+		}
+		wg.Wait()
+		close(primes)
+	}()
 	return primes
 }
 
