cd3df8f3e31aeb116b277faf7883f3307086614a
Magesh Kuppan <tkmagesh77@gmail.com>
2025-04-24 16:17:52 +0530

    middlewares applied

---
+++ 06-http-services/01-net-http/go.mod
@@ -0,0 +1,5 @@
+module http-services-app
+
+go 1.23.3
+
+require github.com/agext/uuid v1.1.0 // indirect
--- 06-http-services/01-net-http/server-03.go
+++ 06-http-services/01-net-http/server-03.go
@@ -38,7 +38,7 @@ func (ps *ProductsStore) GetAll(ctx context.Context) ([]Product, error) {
 	// TODO : implement logging
 	log.Printf("[ProductsStore.GetAll()][%s] - returning products\n", ctx.Value("trace-id"))
 	log.Printf("[ProductsStore.GetAll()][%s] - retrieving products from db\n", ctx.Value("trace-id"))
-	time.Sleep(2 * time.Second) // simulating time consuming db communication
+	time.Sleep(5 * time.Second) // simulating time consuming db communication
 	if ctx.Err() == context.DeadlineExceeded {
 		log.Printf("[ProductsStore.GetAll()][%s] - timeout from db\n", ctx.Value("trace-id"))
 		return nil, errors.New("request timedout")
@@ -52,12 +52,24 @@ func (ps *ProductsStore) Add(ctx context.Context, p Product) {
 	ps.list = append(ps.list, p)
 }
 
+type HandlerFunction func(http.ResponseWriter, *http.Request)
+type MiddlewareFunction func(HandlerFunction) HandlerFunction
+
 type AppServer struct {
-	routes map[string]func(http.ResponseWriter, *http.Request)
+	routes      map[string]HandlerFunction
+	middlewares []MiddlewareFunction
 }
 
-func (appServer *AppServer) Add(pattern string, handlerFn func(http.ResponseWriter, *http.Request)) {
+func (appServer *AppServer) Add(pattern string, handlerFn HandlerFunction) {
+	for i := len(appServer.middlewares) - 1; i >= 0; i-- {
+		middleware := appServer.middlewares[i]
+		handlerFn = middleware(handlerFn)
+	}
 	appServer.routes[pattern] = handlerFn
+}
+
+func (appServer *AppServer) Use(middleware MiddlewareFunction) {
+	appServer.middlewares = append(appServer.middlewares, middleware)
 }
 
 func (appServer *AppServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -70,7 +82,7 @@ func (appServer *AppServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func NewAppServer() *AppServer {
 	return &AppServer{
-		routes: make(map[string]func(http.ResponseWriter, *http.Request)),
+		routes: make(map[string]HandlerFunction),
 	}
 }
 
@@ -121,14 +133,14 @@ func CustomersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Middlewares
-func loggerMiddleware(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func loggerMiddleware(handler HandlerFunction) HandlerFunction {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s - %s - %s\n", r.Context().Value("trace-id"), r.Method, r.URL.Path)
 		handler(w, r)
 	}
 }
 
-func traceMiddleware(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func traceMiddleware(handler HandlerFunction) HandlerFunction {
 	return func(w http.ResponseWriter, r *http.Request) {
 		uuidObj := uuid.New()
 		traceCtx := context.WithValue(r.Context(), "trace-id", uuidObj.String())
@@ -136,13 +148,13 @@ func traceMiddleware(handler func(http.ResponseWriter, *http.Request)) func(http
 	}
 }
 
-func timeoutMiddleware(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func timeoutMiddleware(handler HandlerFunction) HandlerFunction {
 	return func(w http.ResponseWriter, r *http.Request) {
 		timeoutCtx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
 		defer cancel()
 		handler(w, r.WithContext(timeoutCtx))
 		if r.Context().Err() == context.DeadlineExceeded {
-			log.Println("%s - timedout", r.Context().Value("trace-id"))
+			log.Printf("%s - timedout\n", r.Context().Value("trace-id"))
 		}
 	}
 }
@@ -150,9 +162,13 @@ func timeoutMiddleware(handler func(http.ResponseWriter, *http.Request)) func(ht
 func main() {
 
 	appServer := NewAppServer()
-	appServer.Add("/", traceMiddleware(loggerMiddleware(IndexHandler)))
-	appServer.Add("/products", traceMiddleware(loggerMiddleware(timeoutMiddleware(ProductsHandler))))
-	appServer.Add("/customers", traceMiddleware(loggerMiddleware(CustomersHandler)))
+	appServer.Use(traceMiddleware)
+	appServer.Use(loggerMiddleware)
+	appServer.Use(timeoutMiddleware)
+
+	appServer.Add("/", IndexHandler)
+	appServer.Add("/products", ProductsHandler)
+	appServer.Add("/customers", CustomersHandler)
 	if err := http.ListenAndServe(":8080", appServer); err != nil {
 		log.Println("Error :", err)
 	}
